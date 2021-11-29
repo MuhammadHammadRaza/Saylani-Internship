329e27b636fac32325a09d657d8eea8c67a93921
MuhammadHammadRaza <hammadrazashekhani@gmail.com>
2021-11-29 20:00:23 +0500

    added signup page

--- teamreporter-sec-a/src/App.js
+++ teamreporter-sec-a/src/App.js
@@ -1,13 +1,11 @@
-
-import Signup from "./components/Signup";
 import "antd/dist/antd.css";
-import Login from './components/Login';
 import './App.css';
 import Routes from './routes.js'
 
 function App() {
   return (
     <div className="App">
+      <Routes />
     </div>
   );
 }
--- teamreporter-sec-a/src/routes.js
+++ teamreporter-sec-a/src/routes.js
@@ -1,17 +1,14 @@
 import { Routes as AppRoutes, Route } from "react-router-dom"
-import Signup from './components/Signup.js'
-import Login from './components/Login.js'
-import Dashboard from './components/dashboard.js'
-const Routes = () =>{
-   
-   
-   
-    return(
-<AppRoutes>
-    <Route path='/' element={<Login/>}/>
-    <Route path='Home' element={<Dashboard/>}/>
-    <Route path='/Signup' element={<Signup/>}/>
-</AppRoutes>
+import Signup from './pages/Signnup'
+import Login from './pages/Login'
+import Dashboard from './pages/dashboard'
+const Routes = () => {
+    return (
+        <AppRoutes>
+            <Route path='/' element={<Login />} />
+            <Route path='Home' element={<Dashboard />} />
+            <Route path='/Signup' element={<Signup />} />
+        </AppRoutes>
     )
 }
 export default Routes;
